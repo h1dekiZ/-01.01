4d6163b7ebe718bb9d423da85ce30c09d6ae8bd4
h1dekiZ <yano4kin.dima@gmail.com>
2024-11-11 12:05:55 +0300

    Add files via upload

---
+++ wpf5.1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace wpf5._1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Изменение стиля текстового поля, которое получило фокус
+            (sender as FrameworkElement).Style = (Style)Resources["LargeTextBoxStyle"];
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Возвращение стандартного стиля текстовому полю, которое потеряло фокус
+            (sender as FrameworkElement).Style = (Style)Resources["SmallTextBoxStyle"];
+        }
+    }
+}
